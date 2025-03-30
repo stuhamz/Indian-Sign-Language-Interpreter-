f2002a1106e695ad6b9867dea2e008a02a5bb5cd
stuhamz <stu.hamzahh.09@gmail.com>
2025-03-30 05:45:44 +0530

    Add src/__init__.py 1743293743223

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+Indian Sign Language Interpreter
+A machine learning model for interpreting live Indian Sign Language
+using Python, OpenCV, and TensorFlow/Keras.
+"""
+
+__version__ = "1.0.0"
+__author__ = "BLACKBOXAI"
